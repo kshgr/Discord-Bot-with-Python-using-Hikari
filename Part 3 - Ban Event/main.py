553b1228196d7fa7d6cf65d0fe8a0bbe8ff9e647
Kushagra Mittal <kushagram1311@gmail.com>
2022-06-25 13:45:07 +0530

    Update main.py

--- Part 3 - Ban Event/main.py
+++ Part 3 - Ban Event/main.py
@@ -11,25 +11,33 @@ BanEvent
 """
 
 @bot.listen(hikari.BanEvent)
+# Event triggered whenever a user's ban status is modified for a server. 
 async def Ban_Event(event):
-    user = event.user
-
+    user = event.user           # Gives Username#1234 for concerned user.
+    
     print(f"There is an update to {user}'s Ban Status.")
 
 @bot.listen(hikari.BanCreateEvent)
+# Event triggered whenever a user is banned from a server.
 async def Ban_Create(event):
-    guild_id= event.guild_id
-    shard= event.shard
-    user = event.user
-    user_id = event.user_id
+    guild_id= event.guild_id    # Gives a unique numeric Server ID for the concerned server.
+    
+    shard= event.shard          # Gives Shard ID under which the particular instance of bot is being run.
+    
+    user = event.user           # Gives Username#1234 for concerned user.
+    
+    user_id = event.user_id     # Gives a unique numeric User ID for concerned user.
 
     print(f"{user} having {user_id} is banned from Guild - {guild_id} in Shard {shard}.")
 
 @bot.listen(hikari.BanDeleteEvent)
+# Event Triggered whenever a user is unbanned from a server.
 async def Unban(event):
-    guild_id = event.guild_id
-    shard = event.shard
-    user = event.user
+    guild_id= event.guild_id    # Gives a unique numeric Server ID for the concerned server.
+    
+    shard= event.shard          # Gives Shard ID under which the particular instance of bot is being run.
+    
+    user = event.user           # Gives Username#1234 for concerned user.
 
     print(f"{user} has been unbanned from Guild - {guild_id} in Shard - {shard}.")
 
